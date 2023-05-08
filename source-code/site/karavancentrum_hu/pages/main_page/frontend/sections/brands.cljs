442930a5a70a191a28c48132b3d0a9ccc11f5a2b
Peeter95 <szan.peter95@gmail.com>
2023-05-08 21:17:01 +0200

    [Fixed] Removed content(link data) from brand cards, also solved break-line issues here.

--- source-code/site/karavancentrum_hu/pages/main_page/frontend/sections/brands.cljs
+++ source-code/site/karavancentrum_hu/pages/main_page/frontend/sections/brands.cljs
@@ -2,7 +2,53 @@
 (ns site.karavancentrum-hu.pages.main-page.frontend.sections.brands
     (:require [css.api      :as css]
               [re-frame.api :as r]
-              [uri.api      :as uri]))
+              [uri.api      :as uri]
+              [hiccup.api   :as hiccup]
+              [string.api   :as string]
+              [vector.api   :as vector]))
+
+;; WARNING
+;; Fixing line break issues
+;; /n not breaking as intented
+
+(defn gap-items
+  ; @param () n
+  ; @param () delimiter
+  ;
+  ; @usage
+  ; (gap-items [:A :B] :x)
+  ;
+  ; @example
+  ; (gap-items [:A :B :C :D] :x)
+  ; =>
+  ; [:A :x :B :x :C :x :D]
+  ;
+  ; @return (vector)
+  [n delimiter]
+  (letfn [(f [result dex x]
+            (if (= 0 dex)
+              [x]
+              (conj result delimiter x)))]
+    (reduce-kv f [] n)))
+
+(defn parse-newlines
+  ; @param (hiccup) n
+  ;
+  ; @example
+  ; (parse-newlines [:p "Hello world!\nIt's me!"])
+  ; =>
+  ; [:p "Hello world!" [:br] "It's me!"]
+  ;
+  ; @return (hiccup)
+  [n]
+  (letfn [(f0 [element] (reduce f1 [] element))
+          (f1 [element content] (if (string/contains-part? content "\n")
+                                  (as-> content % (string/split % #"\n")
+                                        (gap-items % [:br])
+                                        (vector/concat-items element %))
+                                  (vector/conj-item element content)))]
+    (hiccup/walk n f0)))
+
 
 ;; ----------------------------------------------------------------------------
 ;; ----------------------------------------------------------------------------
@@ -10,11 +56,11 @@
 (defn brand
   [brand-dex {:keys [link-label link description title]}]
   [:div.kc-brand [:p.kc-brand--title         title]
-                 [:div.kc-brand--description description]
+                 (parse-newlines [:div.kc-brand--description description])
                  [:div {:class :kc-brand--link-box}
-                       [:div {}
-                             [:div.kc-brand--label link-label]
-                             [:div.kc-brand--link  link]]
+                      ;;  [:div {}]
+                            ;;  [:div.kc-brand--label link-label]]
+                            ;;  [:div.kc-brand--link  link]]
                        [:a.kc-brand--goto {:href (uri/valid-uri link) :title "Hivatkozás megnyitása" :target "_blank"}
                                           [:span "Megtekintés"]
                                           [:i.fas.fa-arrow-right]]]])
